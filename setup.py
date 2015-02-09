5111b340c0f96b3963697805a56b063d9139f043
Bob Farrell <robertanthonyfarrell@gmail.com>
2015-02-09 15:32:50 +0000

    Add setup.py and initial project dir

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='WebStress',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Bob Farrell',
+      author_email='robertanthonyfarrell@gmail.com',
+      packages=['webstress'],
+     )
